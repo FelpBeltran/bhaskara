604ba756a5eeba259b72c60ad148836faea64320
felipe.mbeltran <felipe.mbeltran@CAS555598W0748.senacsp.edu.br>
2017-03-23 20:22:15 -0300

    Solução de bhaskara as avessas usando next.Double();

---
+++ src/bhaskara/Bhaskara.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bhaskara;
+
+import java.util.Scanner;
+
+public class Bhaskara {
+
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Insira o valor de x1: ");
+        double x1 = console.nextDouble();
+        System.out.println("Insira o valor de x2: ");
+        double x2 = console.nextDouble();
+        System.out.println("Insira o valor de C: ");
+        double c = console.nextDouble();
+        double s = x1 + x2;
+        double p = x1 * x2;
+        double a = c/p;
+        double b = -s*a;
+        System.out.println("A equação que tem raízes" +x1 +" e " + x2 +" é "+ a +" x² "+ b+ " x "+c);
+    }
+
+}
